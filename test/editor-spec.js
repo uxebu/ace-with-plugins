5baca4ea4b5a601800a9062a9133803a9b42321e
Georg Lang <georglang0788@gmx.net>
2014-09-16 10:30:20 +0200

    test to get absolute position of variable in one line

--- test/editor-spec.js
+++ test/editor-spec.js
@@ -1,9 +1,18 @@
 var Editor = require('../src/editor/editor');
 
-describe('Setting up Editor', function () {
-  var ace;
-  var editor = new Editor('editorNode');
-//  var editor = new Editor('editorNode');
-  spyOn(editor, '_init');
-  expect(editor._init).toHaveBeenCalled();
+function calculateAbsolutePositionOfCursor(cursorPosition, lenght){
+
+}
+
+describe('get cursor position', function () {
+  xit('position of str in first line should be 4', function () {
+    var codeAsString = 'var str = 4;';
+
+    var cursorPosition = {
+      row: 0,
+      column: 4
+    }
+    expect(calculateAbsolutePositionOfCursor(cursorPosition, codeAsString.lenght)).toBe(4);
+  });
 });
+
