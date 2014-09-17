c71d883cdc8ba361dc0bc5de1cf2dab631ebd6bd
Georg Lang <georglang0788@gmx.net>
2014-09-17 12:51:23 +0200

    test return five positions

--- test/findSameOccurences-spec.js
+++ test/findSameOccurences-spec.js
@@ -72,11 +72,16 @@ describe('simple source code, with two occurence', function () {
   });
 });
 
-describe('simple source code, with three occurences', function () {
+describe('simple source code, with three or more occurences', function () {
   it('should return three positions', function () {
     var sourceCode = 'var xyz = 0; xyz++; xyz = 4;';
     var currentCursorPosition = 20;
     expect(getPositionOfOccurence(sourceCode, currentCursorPosition)).toEqual([4, 13, 20]);
+  });
+  it('should return five positions', function () {
+    var sourceCode = 'var xyz = 0; xyz++; xyz = 4; xyz++; xyz = 4;';
+    var currentCursorPosition = 20;
+    expect(getPositionOfOccurence(sourceCode, currentCursorPosition)).toEqual([4, 13, 20, 29, 36]);
   });
 });
 
