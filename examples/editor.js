c6a68f8512d04af330bec55276335752402c6ab9
Wolfram Kriesing <wk@uxebu.com>
2014-09-18 17:27:02 +0200

    Let's pass in an instance, not the Class.

--- examples/editor.js
+++ examples/editor.js
@@ -1,6 +1,6 @@
 var Editor = require('../src/editor/editor');
 var Ace = require('../src/editor/ace');
 
-var editor = new Editor(Ace, 'editorNode');
+var editor = new Editor(new Ace('editorNode'));
 editor.setContent('function foo(){ var test = 0;}\nvar test = 4\nvar foo = 42; y = foo * 2; z = foo / 2');
 editor.enableLiveRenaming();
