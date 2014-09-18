6ac1fdf8bab2b30cf0d623c4b292eab0f4a9d9d3
Wolfram Kriesing <wk@uxebu.com>
2014-09-18 15:51:26 +0200

    We have source code as input, API!

--- test/editor/to-absolute-cursor-pos-spec.js
+++ test/editor/to-absolute-cursor-pos-spec.js
@@ -5,11 +5,18 @@ function toAbsoluteCursorPosition(coordinates) {
   return 0;
 }
 
+var sourceCode = [
+  'line 0 ...0123456789',
+  '0123456789 line1 78',
+  'line 2 7890',
+  ''
+].join('\n');
+
 describe('calculate the absolute cursor position from given: row+column and sourcecode', function() {
   it('for 0x0', function() {
-    expect(toAbsoluteCursorPosition({row: 0, column: 0})).toBe(0);
+    expect(toAbsoluteCursorPosition({row: 0, column: 0}), sourceCode).toBe(0);
   });
   it('for 0x10', function() {
-    expect(toAbsoluteCursorPosition({row: 0, column: 10})).toBe(10);
+    expect(toAbsoluteCursorPosition({row: 0, column: 10}), sourceCode).toBe(10);
   });
 });
