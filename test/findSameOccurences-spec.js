8fb410d73615d76887468b3ae55f5229fc9a9eb6
Georg Lang <georglang0788@gmx.net>
2014-09-18 09:00:56 +0200

    test for javascript keywords on cursor position

--- test/findSameOccurences-spec.js
+++ test/findSameOccurences-spec.js
@@ -107,4 +107,21 @@ describe('simple source code, with special chars in occurences', function () {
   });
 });
 
+describe('javascript token on cursor position', function () {
+  it('should return empty array', function () {
+    var sourceCode = 'function foo(){}';
+    var currentCursorPosition = 0;
+    expect(getPositionOfOccurence(sourceCode, currentCursorPosition)).toEqual([]);
+  });
+  it('should return empty array', function () {
+    var sourceCode = 'var xYz = 0; xYz++; xYz = 4;';
+    var currentCursorPosition = 17;
+    expect(getPositionOfOccurence(sourceCode, currentCursorPosition)).toEqual([]);
+  });
+  it('should return two positions', function () {
+    var sourceCode = 'var foo = function(){}; foo();';
+    var currentCursorPosition = 5;
+    expect(getPositionOfOccurence(sourceCode, currentCursorPosition)).toEqual([4, 24]);
+  });
+});
 
