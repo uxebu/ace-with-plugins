8f9e666562ee85c42754df9af7587d0f22c7209d
Wolfram Kriesing <wk@uxebu.com>
2014-09-18 15:54:14 +0200

    Implement for the second line.

--- test/editor/to-absolute-cursor-pos-spec.js
+++ test/editor/to-absolute-cursor-pos-spec.js
@@ -1,22 +1,36 @@
-function toAbsoluteCursorPosition(coordinates) {
+function toAbsoluteCursorPosition(coordinates, sourceCode) {
+  if (coordinates.row == 1) {
+    return sourceCode.split('\n')[0].length;
+  }
   if (coordinates.column > 0) {
     return coordinates.column;
   }
   return 0;
 }
 
+var firstLine = 'line 0 ...0123456789';
 var sourceCode = [
-  'line 0 ...0123456789',
+  firstLine,
   '0123456789 line1 78',
   'line 2 7890',
   ''
 ].join('\n');
 
 describe('calculate the absolute cursor position from given: row+column and sourcecode', function() {
-  it('for 0x0', function() {
-    expect(toAbsoluteCursorPosition({row: 0, column: 0}), sourceCode).toBe(0);
+
+  describe('on the first row', function() {
+    it('for 0x0', function() {
+      expect(toAbsoluteCursorPosition({row: 0, column: 0}, sourceCode)).toBe(0);
+    });
+    it('for 0x10', function() {
+      expect(toAbsoluteCursorPosition({row: 0, column: 10}, sourceCode)).toBe(10);
+    });
   });
-  it('for 0x10', function() {
-    expect(toAbsoluteCursorPosition({row: 0, column: 10}), sourceCode).toBe(10);
+
+  describe('on the second row', function() {
+    it('for 1x0', function() {
+      expect(toAbsoluteCursorPosition({row: 1, column: 0}, sourceCode)).toBe(firstLine.length);
+    });
   });
+
 });
