389f9ebef60528c94042349dcf18e2977e2b1d28
Georg Lang <georglang0788@gmx.net>
2014-09-18 11:50:15 +0200

    get cursor position on with intervall

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -1,36 +1,51 @@
-  function Editor(domNodeId) {
-    this._domNodeId = domNodeId;
-    this._init();
+function Editor(domNodeId) {
+  this._domNodeId = domNodeId;
+  this._init();
+}
+
+Editor.prototype = {
+
+  _init: function () {
+    ace.require("ace/ext/language_tools");
+    var editor = ace.edit(this._domNodeId);
+    editor.getSession().setMode('ace/mode/javascript');
+    editor.setOptions({
+      enableBasicAutocompletion: true
+    });
+
+    editor.getSession().setTabSize(2);
+    document.getElementById(this._domNodeId).style.fontSize = '12px';
+    document.getElementById(this._domNodeId).style.backgroundColor = 'white';
+    this._editor = editor;
+  },
+
+  setContent: function (content) {
+    this._editor.selectAll();
+    this._editor.insert(content);
+  },
+
+  getContent: function () {
+    return this._editor.getValue()
+  },
+
+  insertAtCursorPosition: function (s) {
+    this._editor.insert(s);
+  },
+
+  getCurrentCursorPosition: function () {
+    var current = this._editor.session.doc.positionToIndex(this._editor.selection.getCursor());
+    return current;
+  },
+
+//    editor.onCursorMove(function(pos){ console.log('new cursor position:', pos) })
+  
+  onCursorMove: function (callback) {
+    var that = this;
+    setInterval(function () {
+      var currentPosition = that.getCurrentCursorPosition();
+      callback(currentPosition);
+    }, 200)
   }
+};
 
-  Editor.prototype = {
-
-    _init: function() {
-      ace.require("ace/ext/language_tools");
-      var editor = ace.edit(this._domNodeId);
-      editor.getSession().setMode('ace/mode/javascript');
-      editor.setOptions({
-        enableBasicAutocompletion: true
-      });
-
-      editor.getSession().setTabSize(2);
-      document.getElementById(this._domNodeId).style.fontSize = '12px';
-      document.getElementById(this._domNodeId).style.backgroundColor = 'white';
-      this._editor = editor;
-    },
-
-    setContent: function(content) {
-      this._editor.selectAll();
-      this._editor.insert(content);
-    },
-
-    getContent: function() {
-      return this._editor.getValue()
-    },
-
-    insertAtCursorPosition: function(s) {
-      this._editor.insert(s);
-    }
-  };
-
-  module.exports = Editor;
+module.exports = Editor;
