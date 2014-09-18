68dd49b2bea5ab97ed0b25e46f6b9e7f398211ac
Georg Lang <georglang0788@gmx.net>
2014-09-18 10:58:38 +0200

    outsourced tests for js token

--- test/findSameOccurences-spec.js
+++ test/findSameOccurences-spec.js
@@ -17,24 +17,6 @@
 var getPositionOfOccurence = require('../src/refactoring/renaming/renaming.js');
 
 
-describe('javascript token on cursor position', function () {
-  it('should return empty array', function () {
-    var sourceCode = 'function foo(){}';
-    var currentCursorPosition = 0;
-    expect(getPositionOfOccurence(sourceCode, currentCursorPosition)).toEqual([]);
-  });
-  it('should return empty array', function () {
-    var sourceCode = 'var xYz = 0; xYz++; xYz = 4;';
-    var currentCursorPosition = 17;
-    expect(getPositionOfOccurence(sourceCode, currentCursorPosition)).toEqual([]);
-  });
-  it('should return two positions', function () {
-    var sourceCode = 'var foo = function(){}; foo();';
-    var currentCursorPosition = 5;
-    expect(getPositionOfOccurence(sourceCode, currentCursorPosition)).toEqual([4, 24]);
-  });
-});
-
 describe('find occurences in the right scope', function () {
   it('should find variable in function scope', function () {
     var sourceCode = 'function foo(){var test = 4;} var test = 8;';
---
+++ test/refactoring/renaming/js-keywords.js
@@ -0,0 +1,19 @@
+var getPositionOfOccurence = require('../../../src/refactoring/renaming/renaming.js');
+
+describe('javascript token on cursor position', function () {
+  it('should return empty array', function () {
+    var sourceCode = 'function foo(){}';
+    var currentCursorPosition = 0;
+    expect(getPositionOfOccurence(sourceCode, currentCursorPosition)).toEqual([]);
+  });
+  it('should return empty array', function () {
+    var sourceCode = 'var xYz = 0; xYz++; xYz = 4;';
+    var currentCursorPosition = 17;
+    expect(getPositionOfOccurence(sourceCode, currentCursorPosition)).toEqual([]);
+  });
+  it('should return two positions', function () {
+    var sourceCode = 'var foo = function(){}; foo();';
+    var currentCursorPosition = 5;
+    expect(getPositionOfOccurence(sourceCode, currentCursorPosition)).toEqual([4, 24]);
+  });
+});
