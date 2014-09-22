d2e5c003289c7670571b2581b0007c67d126fd67
Wolfram Kriesing <wk@uxebu.com>
2014-09-22 22:06:59 +0200

    Make renaming work in example, with a hack.

--- examples/editor.js
+++ examples/editor.js
@@ -3,3 +3,8 @@ var Ace = require('../src/editor/ace');
 
 var editor = new Editor(new Ace('editorNode'));
 editor.setContent('function foo(){ var test = 0;}\nvar test = 4\nvar foo = 42; y = foo * 2; z = foo / 2');
+
+document.getElementById('renameButton').addEventListener('click', function() {
+  editor.renameAtCurrentPosition();
+  editor._editor._editor.textInput.focus(); // hack in getting back the focus
+});
