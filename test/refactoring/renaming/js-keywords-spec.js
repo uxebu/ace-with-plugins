e7812dbcd6f1de33bc306c6c3f195d9bd5de95c3
Wolfram Kriesing <wk@uxebu.com>
2014-09-23 10:53:43 +0200

    Improve test name.

--- test/refactoring/renaming/js-keywords-spec.js
+++ test/refactoring/renaming/js-keywords-spec.js
@@ -1,17 +1,17 @@
 var getPositionOfOccurence = require('../../../src/refactoring/renaming.js').getPositionsOfCandidates;
 
 describe('javascript token on cursor position', function () {
-  it('should return empty array', function () {
+  it('should not find function-keyword', function () {
     var sourceCode = 'function foo(){}';
     var currentCursorPosition = 0;
     expect(getPositionOfOccurence(sourceCode, currentCursorPosition)).toEqual([]);
   });
-  it('should return empty array', function () {
+  it('should not find ++', function () {
     var sourceCode = 'var xYz = 0; xYz++; xYz = 4;';
     var currentCursorPosition = 17;
     expect(getPositionOfOccurence(sourceCode, currentCursorPosition)).toEqual([]);
   });
-  it('should return two positions', function () {
+  it('should find a function name', function () {
     var sourceCode = 'var foo = function(){}; foo();';
     var currentCursorPosition = 5;
     expect(getPositionOfOccurence(sourceCode, currentCursorPosition)).toEqual([4, 24]);
