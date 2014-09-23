6ae640cce6abc5404f9f4a61be2896f16ae5f568
Georg Lang <georglang0788@gmx.net>
2014-09-23 16:23:37 +0200

    removed unneeded integration tests

--- test/refactoring/renaming/scope-spec.js
+++ test/refactoring/renaming/scope-spec.js
@@ -11,14 +11,4 @@ describe('find occurences in the right scope', function () {
     var currentCursorPosition = 36;
     expect(getPositionOfOccurence.getPositionOfOccurence(sourceCode, currentCursorPosition)).toEqual([34]);
   });
-  it('should find variables inside and outside of function scope', function () {
-    var sourceCode = 'function foo(){test = 4;} var test = 8;';
-    var currentCursorPosition = 34;
-    expect(getPositionOfOccurence.getPositionOfOccurence(sourceCode, currentCursorPosition)).toEqual([30, 15]);
-  });
-  it('should find two variables in function scope', function () {
-    var sourceCode = 'function foo(){var test = 4; test++;} var test = 8;';
-    var currentCursorPosition = 21;
-    expect(getPositionOfOccurence.getPositionOfOccurence(sourceCode, currentCursorPosition)).toEqual([19, 29]);
-  });
 });
