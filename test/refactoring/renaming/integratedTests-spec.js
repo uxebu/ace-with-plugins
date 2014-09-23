d50d54a01075397a5291b937be0b9d4110eca407
Georg Lang <georglang0788@gmx.net>
2014-09-23 13:05:19 +0200

    removed unnecessary tests

--- test/refactoring/renaming/integratedTests-spec.js
+++ test/refactoring/renaming/integratedTests-spec.js
@@ -7,23 +7,6 @@ Editor.prototype.getIdentifiedReferences = function (codeAsString, position) {
 
 
 describe('mocking getIdentifiedReferences', function() {
-  it('should call getIdentifiedReferences', function(){
-    var editor = new Editor();
-
-    spyOn(editor, 'getIdentifiedReferences');
-    editor.getIdentifiedReferences();
-
-    expect(editor.getIdentifiedReferences).toHaveBeenCalled();
-  });
-  it('should call getIdentifiedReferences with empty string', function(){
-    var editor = new Editor();
-    var code = ' ';
-    var currentCursorPosition = 0;
-
-    spyOn(editor, 'getIdentifiedReferences');
-    editor.getIdentifiedReferences(code, currentCursorPosition);
-    expect(editor.getIdentifiedReferences).toHaveBeenCalledWith(' ', 0);
-  });
   it('should call getIdentifiedReferences with string', function(){
     var editor = new Editor();
     var code = 'foo';
