7e871eff52a277bfe21193aadb16872784b6027a
Wolfram Kriesing <wk@uxebu.com>
2014-09-23 14:59:11 +0200

    Improve naming.

--- src/refactoring/renaming.js
+++ src/refactoring/renaming.js
@@ -16,16 +16,17 @@ function _extractPositionsFromReferences(references) {
 }
 
 var getCursorPositions = function(sourceCode, cursorPosition) {
-  // Use exports explicitly, so we can mock it in the tests. Better ideas?
+  // Use `exports.*` explicitly, so we can mock it in the tests. Better ideas?
   var positions = exports.getPositionsOfCandidates(sourceCode, cursorPosition);
   if (positions.indexOf(cursorPosition) == -1) {
-    var diff = _getClosestDiff(positions, cursorPosition);
-    positions = positions.map(function(pos) { return pos + diff; });
+    var offset = _getClosestValueInArray(positions, cursorPosition);
+    function addOffsetToPosition(pos) { return pos + offset; }
+    positions = positions.map(addOffsetToPosition);
   }
   return positions;
 };
 
-function _getClosestDiff(values, value) {
+function _getClosestValueInArray(values, value) {
   function calculateDistance(aValue) { return value - aValue; }
   function aboveZero(value) { return value > 0 }
   function numericSort(a, b) { return a - b; }
