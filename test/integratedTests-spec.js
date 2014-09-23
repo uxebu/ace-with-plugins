93b113508673a7bb676a1b82566693c4092e663e
Georg Lang <georglang0788@gmx.net>
2014-09-23 10:31:37 +0200

    added test call getIdentifiedReferences with string

--- test/integratedTests-spec.js
+++ test/integratedTests-spec.js
@@ -5,6 +5,7 @@ Editor.prototype.getIdentifiedReferences = function (codeAsString, position) {}
 
 
 describe('mocking getIdentifiedReferences', function() {
+
   it('should call getIdentifiedReferences', function(){
     var editor = new Editor();
 
@@ -22,4 +23,14 @@ describe('mocking getIdentifiedReferences', function() {
     editor.getIdentifiedReferences(code, currentCursorPosition);
     expect(editor.getIdentifiedReferences).toHaveBeenCalledWith(' ', 0);
   });
+  it('should call getIdentifiedReferences with string', function(){
+    var editor = new Editor();
+    var code = 'foo';
+    var currentCursorPosition = 0;
+
+    spyOn(editor, 'getIdentifiedReferences');
+    editor.getIdentifiedReferences(code, currentCursorPosition);
+    expect(editor.getIdentifiedReferences).toHaveBeenCalledWith(code, currentCursorPosition);
+  });
+
 });
