0353bd6a0d74433f3782d7152ef2dce43af7241d
Georg Lang <georglang0788@gmx.net>
2014-09-23 16:25:40 +0200

    test return five array elements and renaming

--- test/refactoring/renaming/integratedTests-spec.js
+++ test/refactoring/renaming/integratedTests-spec.js
@@ -1,8 +1,8 @@
 var renaming = require('../../../src/refactoring/renaming');
 var esRefactorInterface = require('../../../src/refactoring/esRefactorInterface');
 
-describe('mocking getPositionsOfReferences', function() {
-  it('should call getPositionsOfReferences with string', function(){
+describe('mocking getPositionsOfReferences', function () {
+  it('should check if getPositionsOfReferences is called with the right parameters', function () {
     var code = 'foo';
     var currentCursorPosition = 0;
 
@@ -10,15 +10,11 @@ describe('mocking getPositionsOfReferences', function() {
     renaming.getPositionOfOccurence(code, currentCursorPosition);
     expect(esRefactorInterface.getPositionsOfReferences).toHaveBeenCalledWith(code, currentCursorPosition);
   });
-});
+  it('should return five array elements', function () {
+    var code = 'var foo';
+    var currentCursorPosition = 4;
 
-describe('stub getPositionsOfReferences', function() {
-  it('should return one array element', function(){
-    var code = 'foo';
-    var currentCursorPosition = 0;
-
-    spyOn(esRefactorInterface, 'getPositionsOfReferences').andReturn([1]);
-    renaming.getPositionOfOccurence(code, currentCursorPosition);
-    expect(esRefactorInterface.getPositionsOfReferences()).toEqual([1]);
+    spyOn(esRefactorInterface, 'getPositionsOfReferences').andReturn([1, 2, 3, 4, 5]);
+    expect(renaming.getPositionOfOccurence(code, currentCursorPosition)).toEqual([1, 2, 3, 4, 5]);
   });
 });
