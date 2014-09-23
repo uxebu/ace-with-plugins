8b759a91b90aa3aee1c03ec1f058f5342cdfbccb
Georg Lang <georglang0788@gmx.net>
2014-09-23 14:59:43 +0200

    test should return one array element

--- test/refactoring/renaming/integratedTests-spec.js
+++ test/refactoring/renaming/integratedTests-spec.js
@@ -7,8 +7,18 @@ describe('mocking getPositionsOfReferences', function() {
     var currentCursorPosition = 0;
 
     spyOn(esRefactorInterface, 'getPositionsOfReferences');
-
     renaming.getPositionOfOccurence(code, currentCursorPosition);
     expect(esRefactorInterface.getPositionsOfReferences).toHaveBeenCalledWith(code, currentCursorPosition);
+  });
+});
+
+describe('stub getPositionsOfReferences', function() {
+  it('should return one array element', function(){
+    var code = 'foo';
+    var currentCursorPosition = 0;
+
+    spyOn(esRefactorInterface, 'getPositionsOfReferences').andReturn([1]);
+    renaming.getPositionOfOccurence(code, currentCursorPosition);
+    expect(esRefactorInterface.getPositionsOfReferences()).toEqual([1]);
   });
 });
