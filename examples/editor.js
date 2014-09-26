19e6f760a449523694fe93fe5b3a626d796eba1a
Georg Lang <georglang0788@gmx.net>
2014-09-26 10:38:06 +0200

    highlights the new name, if a variable is renamed

--- examples/editor.js
+++ examples/editor.js
@@ -15,7 +15,6 @@ document.getElementById('renameButton').addEventListener('click', function () {
 
 document.getElementById('removeHighlightingButton').addEventListener('click', function () {
   editor.removeHighlightedOccurences();
-  editor.placeCursorsForRenaming();
   editor._editor._editor.textInput.focus(); // hack in getting back the focus
 });
 
@@ -23,11 +22,19 @@ var renamingIsActive = false;
 var onCursorChange;
 
 document.getElementById('activateRenaming').addEventListener('click', function () {
+  renamingIsActive = true;
   onCursorChange = window.setInterval(function () {
-    console.log('cursorchange active');
-  }, 1000);
+    if (renamingIsActive) {
+      editor.removeHighlightedOccurences();
+      editor.placeCursorsForRenaming();
+      editor._editor._editor.textInput.focus(); // hack in getting back the focus
+      editor.highlightOccurences();
+      editor._editor._editor.textInput.focus(); // hack in getting back the focus
+    }
+  }, 100);
 });
 
 document.getElementById('deactivateRenaming').addEventListener('click', function () {
+  renamingIsActive = false;
   clearInterval(onCursorChange);
 });
