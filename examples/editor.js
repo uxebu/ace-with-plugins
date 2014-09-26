9e8ad9e5c79701db2d9fc1b6443f1de366a4b868
Georg Lang <georglang0788@gmx.net>
2014-09-26 10:03:01 +0200

    simulate activate,deactivate renaming

--- examples/editor.js
+++ examples/editor.js
@@ -6,7 +6,7 @@ ace.setDomNodeId('editorNode');
 var editor = new Editor(ace);
 editor.setContent('function foo(){ var test = 0;}\nvar test = 4\nvar foo = 42; y = foo * 2; z = foo / 2');
 
-document.getElementById('renameButton').addEventListener('click', function() {
+document.getElementById('renameButton').addEventListener('click', function () {
   editor.removeHighlightedOccurences();
   editor.placeCursorsForRenaming();
   editor.highlightOccurences();
@@ -17,4 +17,17 @@ document.getElementById('removeHighlightingButton').addEventListener('click', fu
   editor.removeHighlightedOccurences();
   editor.placeCursorsForRenaming();
   editor._editor._editor.textInput.focus(); // hack in getting back the focus
+});
+
+var renamingIsActive = false;
+var onCursorChange;
+
+document.getElementById('activateRenaming').addEventListener('click', function () {
+  onCursorChange = window.setInterval(function () {
+    console.log('cursorchange active');
+  }, 1000);
+});
+
+document.getElementById('deactivateRenaming').addEventListener('click', function () {
+  clearInterval(onCursorChange);
 });
