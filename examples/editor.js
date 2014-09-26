e5223fd6e70ac76e5a336ac9902fa4d1fb6e91d2
Georg Lang <georglang0788@gmx.net>
2014-09-26 09:25:36 +0200

    setting and removing highlighting at the right time

--- examples/editor.js
+++ examples/editor.js
@@ -7,6 +7,7 @@ var editor = new Editor(ace);
 editor.setContent('function foo(){ var test = 0;}\nvar test = 4\nvar foo = 42; y = foo * 2; z = foo / 2');
 
 document.getElementById('renameButton').addEventListener('click', function() {
+  editor.removeHighlightedOccurences();
   editor.placeCursorsForRenaming();
   editor.highlightOccurences();
   editor._editor._editor.textInput.focus(); // hack in getting back the focus
@@ -14,4 +15,6 @@ document.getElementById('renameButton').addEventListener('click', function() {
 
 document.getElementById('removeHighlightingButton').addEventListener('click', function () {
   editor.removeHighlightedOccurences();
-})
+  editor.placeCursorsForRenaming();
+  editor._editor._editor.textInput.focus(); // hack in getting back the focus
+});
