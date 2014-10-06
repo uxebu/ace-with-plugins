5383d44f102461a4b08793feb7ed83424f7c942c
Wolfram Kriesing <wk@uxebu.com>
2014-10-06 08:30:17 +0200

    Provide a bit more (complex) example code.

--- examples/editor.js
+++ examples/editor.js
@@ -4,7 +4,7 @@ var Ace = require('../src/editor/ace');
 var ace = new Ace();
 ace.setDomNodeId('editorNode');
 var editor = new Editor(ace);
-editor.setContent('function foo(){ var test = 0;}\nvar test = 4\nvar foo = 42; y = foo * 2; z = foo / 2');
+editor.setContent('function func(){ var test = 0;}\nfunc();\nvar test = 4\nvar foo = 42,\n    bar;\ny = bar * 2;\nz = foo / 2;\nfoo = bar * 2;');
 
 document.getElementById('renameButton').addEventListener('click', function() {
   editor.placeCursorsForRenaming();
