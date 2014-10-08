2be96cc4c45a54acf7d535b1651633c652c695c5
Wolfram Kriesing <wk@uxebu.com>
2014-10-08 17:28:04 +0200

    Move tests to where the source lives.

--- src/util/test/to-absolute-cursor-pos-spec.js
+++ src/util/test/to-absolute-cursor-pos-spec.js
@@ -1,4 +1,4 @@
-var toAbsoluteCursorPosition = require('../../util/cursor-position').toAbsolute;
+var toAbsoluteCursorPosition = require('../cursor-position').toAbsolute;
 
 var firstLine = 'line 0 ...0123456789';
 var secondLine = '0123456789 line1 78';
